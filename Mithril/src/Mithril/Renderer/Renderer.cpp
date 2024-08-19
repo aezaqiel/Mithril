37e421fc2622bcdadedb16423b29b3a00938b0df
Aliff Ezaqiel Zaffrulla <aezaqiel@gmail.com>
2024-08-19 17:33:08 +0800

    Logical device && graphics & present queue

--- Mithril/src/Mithril/Renderer/Renderer.cpp
+++ Mithril/src/Mithril/Renderer/Renderer.cpp
@@ -1,10 +1,15 @@
 #include "Renderer.hpp"
 
 #include <sstream>
+#include <set>
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+#include "Mithril/Core/Application.hpp"
 #include "Mithril/Core/Logger.hpp"
 
 namespace Mithril {
@@ -61,11 +66,15 @@ namespace Mithril {
         }
         
         CreateInstance();
+        CreateSurface();
         PickPhysicalDevice();
+        CreateLogicalDevice();
     }
 
     Renderer::~Renderer()
     {
+        vkDestroyDevice(m_Device, m_Allocator);
+        vkDestroySurfaceKHR(m_Instance, m_Surface, m_Allocator);
 #ifndef NDEBUG
         vkDestroyDebugUtilsMessengerEXT(m_Instance, m_DebugMessenger, m_Allocator);
 #endif
@@ -141,6 +150,13 @@ namespace Mithril {
 #endif
     }
 
+    void Renderer::CreateSurface()
+    {
+        if (glfwCreateWindowSurface(m_Instance, static_cast<GLFWwindow*>(Application::Instance().GetWindow()->Native()), m_Allocator, &m_Surface) != VK_SUCCESS) {
+            M_CORE_ERROR("Failed to create surface");
+        }
+    }
+
     void Renderer::PickPhysicalDevice()
     {
         m_PhysicalDevice = VK_NULL_HANDLE;
@@ -180,6 +196,7 @@ namespace Mithril {
         M_CORE_DEBUG("Checking physical device {}", properties.deviceName);
 
         if (properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+            M_CORE_TRACE("Not a discrete gpu");
             return false;
         }
 
@@ -187,10 +204,152 @@ namespace Mithril {
         vkGetPhysicalDeviceFeatures(device, &features);
 
         if (!features.geometryShader) {
+            M_CORE_TRACE("Geometry shader not supported");
+            return false;
+        }
+
+        QueueFamilyIndices indices = FindQueueFamilies(device);
+
+        if (!indices.GraphicsFamily.has_value()) {
+            M_CORE_TRACE("Does not support graphics");
+            return false;
+        }
+
+        if (!indices.PresentFamily.has_value()) {
+            M_CORE_TRACE("Does not support surface");
             return false;
         }
 
         return true;
+    }
+
+    Renderer::QueueFamilyIndices Renderer::FindQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+
+        uint32_t count = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &count, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(count);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &count, queueFamilies.data());
+
+        int32_t i = 0;
+        for (const auto& family: queueFamilies) {
+            if (family.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.GraphicsFamily = i;
+            }
+
+            VkBool32 presentSupport = VK_FALSE;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, m_Surface, &presentSupport);
+
+            if (presentSupport == VK_TRUE) {
+                indices.PresentFamily = i;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    void Renderer::CreateLogicalDevice()
+    {
+        QueueFamilyIndices indices = FindQueueFamilies(m_PhysicalDevice);
+
+        std::set<uint32_t> queueFamilies = {indices.GraphicsFamily.value(), indices.PresentFamily.value()};
+
+        float priority[] = { 1.0f };
+
+        std::vector<VkDeviceQueueCreateInfo> queueInfos;
+        for (uint32_t family : queueFamilies) {
+            VkDeviceQueueCreateInfo queueInfo;
+            queueInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueInfo.pNext = nullptr;
+            queueInfo.flags = 0;
+            queueInfo.queueFamilyIndex = family;
+            queueInfo.queueCount = 1;
+            queueInfo.pQueuePriorities = priority;
+
+            queueInfos.push_back(queueInfo);
+        }
+
+        VkPhysicalDeviceFeatures features;
+        features.robustBufferAccess = VK_FALSE;
+        features.fullDrawIndexUint32 = VK_FALSE;
+        features.imageCubeArray = VK_FALSE;
+        features.independentBlend = VK_FALSE;
+        features.geometryShader = VK_FALSE;
+        features.tessellationShader = VK_FALSE;
+        features.sampleRateShading = VK_FALSE;
+        features.dualSrcBlend = VK_FALSE;
+        features.logicOp = VK_FALSE;
+        features.multiDrawIndirect = VK_FALSE;
+        features.drawIndirectFirstInstance = VK_FALSE;
+        features.depthClamp = VK_FALSE;
+        features.depthBiasClamp = VK_FALSE;
+        features.fillModeNonSolid = VK_FALSE;
+        features.depthBounds = VK_FALSE;
+        features.wideLines = VK_FALSE;
+        features.largePoints = VK_FALSE;
+        features.alphaToOne = VK_FALSE;
+        features.multiViewport = VK_FALSE;
+        features.samplerAnisotropy = VK_FALSE;
+        features.textureCompressionETC2 = VK_FALSE;
+        features.textureCompressionASTC_LDR = VK_FALSE;
+        features.textureCompressionBC = VK_FALSE;
+        features.occlusionQueryPrecise = VK_FALSE;
+        features.pipelineStatisticsQuery = VK_FALSE;
+        features.vertexPipelineStoresAndAtomics = VK_FALSE;
+        features.fragmentStoresAndAtomics = VK_FALSE;
+        features.shaderTessellationAndGeometryPointSize = VK_FALSE;
+        features.shaderImageGatherExtended = VK_FALSE;
+        features.shaderStorageImageExtendedFormats = VK_FALSE;
+        features.shaderStorageImageMultisample = VK_FALSE;
+        features.shaderStorageImageReadWithoutFormat = VK_FALSE;
+        features.shaderStorageImageWriteWithoutFormat = VK_FALSE;
+        features.shaderUniformBufferArrayDynamicIndexing = VK_FALSE;
+        features.shaderSampledImageArrayDynamicIndexing = VK_FALSE;
+        features.shaderStorageBufferArrayDynamicIndexing = VK_FALSE;
+        features.shaderStorageImageArrayDynamicIndexing = VK_FALSE;
+        features.shaderClipDistance = VK_FALSE;
+        features.shaderCullDistance = VK_FALSE;
+        features.shaderFloat64 = VK_FALSE;
+        features.shaderInt64 = VK_FALSE;
+        features.shaderInt16 = VK_FALSE;
+        features.shaderResourceResidency = VK_FALSE;
+        features.shaderResourceMinLod = VK_FALSE;
+        features.sparseBinding = VK_FALSE;
+        features.sparseResidencyBuffer = VK_FALSE;
+        features.sparseResidencyImage2D = VK_FALSE;
+        features.sparseResidencyImage3D = VK_FALSE;
+        features.sparseResidency2Samples = VK_FALSE;
+        features.sparseResidency4Samples = VK_FALSE;
+        features.sparseResidency8Samples = VK_FALSE;
+        features.sparseResidency16Samples = VK_FALSE;
+        features.sparseResidencyAliased = VK_FALSE;
+        features.variableMultisampleRate = VK_FALSE;
+        features.inheritedQueries = VK_FALSE;
+
+        VkDeviceCreateInfo createInfo;
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pNext = nullptr;
+        createInfo.flags = 0;
+        createInfo.queueCreateInfoCount = queueInfos.size();
+        createInfo.pQueueCreateInfos = queueInfos.data();
+        createInfo.enabledLayerCount = m_Layers.size();
+        createInfo.ppEnabledLayerNames = m_Layers.data();
+        createInfo.enabledExtensionCount = 0;
+        createInfo.ppEnabledExtensionNames = nullptr;
+        createInfo.pEnabledFeatures = &features;
+
+        if (vkCreateDevice(m_PhysicalDevice, &createInfo, m_Allocator, &m_Device) != VK_SUCCESS) {
+            M_CORE_ERROR("Failed to create logical device");
+        }
+
+        volkLoadDevice(m_Device);
+
+        vkGetDeviceQueue(m_Device, indices.GraphicsFamily.value(), 0, &m_GraphicsQueue);
+        vkGetDeviceQueue(m_Device, indices.PresentFamily.value(), 0, &m_PresentQueue);
     }
 
 }
--- Mithril/src/Mithril/Renderer/Renderer.hpp
+++ Mithril/src/Mithril/Renderer/Renderer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <optional>
 
 #include <volk.h>
 
@@ -15,11 +16,22 @@ namespace Mithril {
         void Draw();
 
         void Resize(uint32_t width, uint32_t height);
+    
+    private:
+        struct QueueFamilyIndices
+        {
+            std::optional<uint32_t> GraphicsFamily;
+            std::optional<uint32_t> PresentFamily;
+        };
+
 
     private:
         void CreateInstance();
+        void CreateSurface();
         void PickPhysicalDevice();
         bool DeviceSuitable(VkPhysicalDevice device);
+        QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+        void CreateLogicalDevice();
 
     private:
         VkAllocationCallbacks* m_Allocator { nullptr };
@@ -29,7 +41,12 @@ namespace Mithril {
 
         VkInstance m_Instance;
         VkDebugUtilsMessengerEXT m_DebugMessenger;
+        VkSurfaceKHR m_Surface;
         VkPhysicalDevice m_PhysicalDevice;
+        QueueFamilyIndices m_Indices;
+        VkQueue m_GraphicsQueue;
+        VkQueue m_PresentQueue;
+        VkDevice m_Device;
     };
 
 }
